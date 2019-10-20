9a0c26ec190018656109d8006eab830095165017
Peter Heylin <peter@peterheylin.com>
2019-10-20 13:08:05 +0100

    20191020 updated script

--- sendSMS.py
+++ sendSMS.py
@@ -7,14 +7,20 @@ authToken = os.environ["TWILIO_AUTH_TOKEN"]
 
 client = Client(accountSID, authToken)
 
-message = """
+grpMsg = """
 Reminder: You registered for CoderDojo Kells Autumn
 Season starting this Sat. 3:30-5pm for 8 weeks. See you then.
 Qus: info@coderdojokells.com
 """
 
+singleMsg = """
+You expressed an interest in our Autumn season. 
+We now have a space. If you are still interested can 
+you please confirm by emailing info@coderdojokells.com 
+"""
+
 # read in phone number list
-with open('testdata.csv', mode='r') as infile:
+with open('extradata.csv', mode='r') as infile:
     reader = csv.reader(infile)
     phoneNumbers = {rows[0]:rows[1] for rows in reader}
 
@@ -23,7 +29,7 @@ for name,number in phoneNumbers.items():
     client.messages.create(
         to='+'+number,
         from_="CoderDojo", #"+16508305452",
-        body=name+', '+message 
+        body=name+', '+singleMsg 
     )
     
 
