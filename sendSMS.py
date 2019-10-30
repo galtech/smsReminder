2bdc6fd2423660158a4f02d47da2b572aaa54688
Peter Heylin <peter@peterheylin.com>
2019-10-30 16:57:25 +0000

    20191030 updated script to replace hard coded msgs and filenames with args

--- sendSMS.py
+++ sendSMS.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from twilio.rest import Client
 import csv
 
@@ -7,29 +8,25 @@ authToken = os.environ["TWILIO_AUTH_TOKEN"]
 
 client = Client(accountSID, authToken)
 
-grpMsg = """
-Reminder: You registered for CoderDojo Kells Autumn
-Season starting this Sat. 3:30-5pm for 8 weeks. See you then.
-Qus: info@coderdojokells.com
-"""
-
-singleMsg = """
-You expressed an interest in our Autumn season. 
-We now have a space. If you are still interested can 
-you please confirm by emailing info@coderdojokells.com 
-"""
-
 # read in phone number list
-with open('extradata.csv', mode='r') as infile:
+with open(sys.argv[1], mode='r') as infile:
     reader = csv.reader(infile)
     phoneNumbers = {rows[0]:rows[1] for rows in reader}
+
+
+# read in custom SMS message
+with open(sys.argv[2], mode='r') as infiletwo:
+    externalMsg = infiletwo.read()
+
+# read in a short From: name
+fromName = sys.argv[3]
 
 # send text to each number 
 for name,number in phoneNumbers.items():
     client.messages.create(
         to='+'+number,
-        from_="CoderDojo", #"+16508305452",
-        body=name+', '+singleMsg 
+        from_=fromName, #"CoderDojo" "+16508305452",
+        body=name+', '+externalMsg 
     )
     
 
